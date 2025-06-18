ac032deb46764dd719bc28c1310a4a062d8c198e
Joe P Shoulak <1021647+JoePShoulak@users.noreply.github.com>
2025-06-18 00:31:25 -0500

    popup polish

--- src/popup.cpp
+++ src/popup.cpp
@@ -2,31 +2,24 @@
 #include "colors.h"
 #include "config.h"
 
-static void ok_event_cb(lv_event_t *e) {
+static void ok_event_cb(lv_event_t *e)
+{
   lv_obj_t *overlay = static_cast<lv_obj_t *>(lv_event_get_user_data(e));
-  if (overlay) {
-    lv_obj_t *tile = lv_obj_get_parent(overlay);
-    if (tile) {
-      lv_obj_t *tv = lv_obj_get_parent(tile);
-      if (tv)
-        lv_obj_set_scroll_dir(tv, LV_DIR_HOR);
-    }
-    lv_obj_del(overlay);
-  }
+  lv_obj_t *tile = lv_obj_get_parent(overlay);
+  lv_obj_t *tile_view = lv_obj_get_parent(tile);
+
+  lv_obj_set_scroll_dir(tile_view, LV_DIR_HOR);
+  lv_obj_del(overlay);
 }
 
-lv_obj_t *show_error_popup(lv_obj_t *parent_tile, const char *msg) {
-  if (!parent_tile)
-    return nullptr;
-
-  lv_obj_t *tv = lv_obj_get_parent(parent_tile);
-  if (tv)
-    lv_obj_set_scroll_dir(tv, LV_DIR_NONE);
+lv_obj_t *show_error_popup(lv_obj_t *parent_tile, const char *msg)
+{
+  lv_obj_t *tile_view = lv_obj_get_parent(parent_tile);
+  lv_obj_set_scroll_dir(tile_view, LV_DIR_NONE);
 
   lv_obj_t *overlay = lv_obj_create(parent_tile);
   lv_obj_remove_style_all(overlay);
-  lv_obj_set_size(overlay, lv_obj_get_width(parent_tile),
-                  lv_obj_get_height(parent_tile));
+  lv_obj_set_size(overlay, lv_obj_get_width(parent_tile), lv_obj_get_height(parent_tile));
   lv_obj_set_style_bg_color(overlay, BLACK, 0);
   lv_obj_set_style_bg_opa(overlay, LV_OPA_50, 0);
   lv_obj_add_flag(overlay, LV_OBJ_FLAG_CLICKABLE);
@@ -38,8 +31,7 @@ lv_obj_t *show_error_popup(lv_obj_t *parent_tile, const char *msg) {
   // use flex column layout
   lv_obj_set_layout(overlay, LV_LAYOUT_FLEX);
   lv_obj_set_flex_flow(overlay, LV_FLEX_FLOW_COLUMN);
-  lv_obj_set_flex_align(overlay, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER,
-                        LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_flex_align(overlay, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
 
   lv_obj_t *box = lv_obj_create(overlay);
   lv_obj_remove_style_all(box);
@@ -51,8 +43,7 @@ lv_obj_t *show_error_popup(lv_obj_t *parent_tile, const char *msg) {
   lv_obj_set_style_pad_row(box, 20, 0);
   lv_obj_set_layout(box, LV_LAYOUT_FLEX);
   lv_obj_set_flex_flow(box, LV_FLEX_FLOW_COLUMN);
-  lv_obj_set_flex_align(box, LV_FLEX_ALIGN_SPACE_AROUND, LV_FLEX_ALIGN_CENTER,
-                        LV_FLEX_ALIGN_CENTER);
+  lv_obj_set_flex_align(box, LV_FLEX_ALIGN_SPACE_AROUND, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
   lv_obj_center(box);
 
   lv_obj_t *label = lv_label_create(box);
@@ -68,25 +59,22 @@ lv_obj_t *show_error_popup(lv_obj_t *parent_tile, const char *msg) {
   lv_obj_set_style_radius(btn, 5, 0);
   lv_obj_set_style_bg_color(btn, GREEN, 0);
   lv_obj_set_style_bg_opa(btn, LV_OPA_COVER, 0);
+  lv_obj_add_event_cb(btn, ok_event_cb, LV_EVENT_CLICKED, overlay);
 
   lv_obj_t *btn_lbl = lv_label_create(btn);
   lv_label_set_text(btn_lbl, "OK");
   lv_obj_center(btn_lbl);
 
-  lv_obj_add_event_cb(btn, ok_event_cb, LV_EVENT_CLICKED, overlay);
-
   return overlay;
 }
 
-lv_obj_t *show_fullscreen_popup(lv_obj_t *parent) {
+lv_obj_t *show_fullscreen_popup(lv_obj_t *parent)
+{
   lv_obj_t *host = parent ? parent : lv_layer_top();
-  if (!host)
-    return nullptr;
 
   lv_obj_t *overlay = lv_obj_create(host);
   lv_obj_remove_style_all(overlay);
-  lv_obj_set_size(overlay, lv_disp_get_hor_res(nullptr),
-                  lv_disp_get_ver_res(nullptr));
+  lv_obj_set_size(overlay, lv_disp_get_hor_res(nullptr), lv_disp_get_ver_res(nullptr));
   lv_obj_set_style_bg_color(overlay, BLACK, 0);
   lv_obj_set_style_bg_opa(overlay, LV_OPA_COVER, 0);
   lv_obj_set_style_pad_all(overlay, 0, 0);
@@ -96,8 +84,7 @@ lv_obj_t *show_fullscreen_popup(lv_obj_t *parent) {
   lv_obj_move_foreground(overlay);
 
   lv_indev_t *indev = lv_indev_get_act();
-  if (indev)
-    lv_indev_reset(indev, overlay);
+  lv_indev_reset(indev, overlay);
 
   return overlay;
 }
